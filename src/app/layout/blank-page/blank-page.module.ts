4810d3e39ac43d39377b6fc363495bedd4a42e5b
Shao Xianhui <huihui6007@163.com>
2018-08-18 17:03:01 +0800

    集成PrimeNG

--- src/app/layout/blank-page/blank-page.module.ts
+++ src/app/layout/blank-page/blank-page.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
 
 import { BlankPageRoutingModule } from './blank-page-routing.module';
 import { BlankPageComponent } from './blank-page.component';
 
 @NgModule({
-    imports: [CommonModule, BlankPageRoutingModule],
+imports: [
+        CommonModule,
+        BlankPageRoutingModule,
+        FormsModule,
+        TableModule,
+        HttpClientModule,
+        InputTextModule,
+        DialogModule,
+        ButtonModule
+        ],
     declarations: [BlankPageComponent]
 })
 export class BlankPageModule {}
